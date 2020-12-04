8c68d74e665a6aff83a4b858fe605fa6ef1eff46
joshvanl <vleeuwenjoshua@gmail.com>
2020-12-04 11:15:05 +0000

    Adds mtls end to end tests
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

--- test/e2e/suite/doc.go
+++ test/e2e/suite/doc.go
@@ -1,6 +1,7 @@
 package suite
 
 import (
+	_ "github.com/cert-manager/istio-csr/test/e2e/suite/mtls"
 	_ "github.com/cert-manager/istio-csr/test/e2e/suite/namespace"
 	_ "github.com/cert-manager/istio-csr/test/e2e/suite/request"
 )
---
+++ test/e2e/suite/mtls/mtls.go
@@ -0,0 +1,143 @@
+package mtls
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/cert-manager/istio-csr/test/e2e/framework"
+)
+
+const (
+	kubectlBin = "../../bin/kubectl"
+)
+
+var _ = framework.CasesDescribe("mTLS correctness", func() {
+	f := framework.NewDefaultFramework("mtls-correctness")
+
+	var (
+		namespaces = []struct {
+			name   string
+			inject bool
+		}{
+			{
+				name:   "foo",
+				inject: true,
+			},
+			{
+				name:   "bar",
+				inject: true,
+			},
+			{
+				name:   "legacy",
+				inject: false,
+			},
+		}
+
+		ctx = context.Background()
+	)
+
+	JustBeforeEach(func() {
+		By("creating foo, bar, and legacy namespaces with resources")
+
+		for _, ns := range namespaces {
+			By(fmt.Sprintf(
+				"creating %s namespace with deployments, inject=%t",
+				ns.name, ns.inject,
+			))
+
+			injectEnabled := "enabled"
+			if !ns.inject {
+				injectEnabled = "disabled"
+			}
+
+			_, err := f.KubeClientSet.CoreV1().Namespaces().Create(ctx, &corev1.Namespace{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: ns.name,
+					Labels: map[string]string{
+						"istio-injection": injectEnabled,
+					},
+				},
+			}, metav1.CreateOptions{})
+			Expect(err).NotTo(HaveOccurred())
+
+			cmd := exec.Command(kubectlBin, "apply", "-n"+ns.name, "-f", "./manifests/.")
+			cmd.Stdout = GinkgoWriter
+			cmd.Stderr = GinkgoWriter
+			Expect(cmd.Run()).NotTo(HaveOccurred())
+		}
+
+		for _, ns := range namespaces {
+			By(fmt.Sprintf("waiting for pods in %q namespace to become ready", ns.name))
+			Expect(f.Helper().WaitForPodsReady(ns.name, time.Minute*3)).NotTo(HaveOccurred())
+		}
+	})
+
+	JustAfterEach(func() {
+		By("deleting foo, bar, and legacy namespaces with resources")
+
+		for _, ns := range namespaces {
+			By(fmt.Sprintf(
+				"deleting %s namespace with deployments, inject=%t",
+				ns.name, ns.inject,
+			))
+
+			err := f.KubeClientSet.CoreV1().Namespaces().Delete(ctx, ns.name, metav1.DeleteOptions{})
+			Expect(err).NotTo(HaveOccurred())
+		}
+	})
+
+	It("it should enforce mTLS, and fail for non-mTLS to mTLS", func() {
+		for _, originNs := range namespaces {
+			By(fmt.Sprintf(
+				"checking mTLS for %s namespace, inject=%t",
+				originNs.name, originNs.inject,
+			))
+
+			originPods, err := f.KubeClientSet.CoreV1().Pods(originNs.name).List(ctx, metav1.ListOptions{
+				LabelSelector: "app=sleep",
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			if len(originPods.Items) != 1 {
+				Expect(fmt.Errorf("expected single sleep pod in ns %q, got=%d", originNs.name, len(originPods.Items))).NotTo(HaveOccurred())
+			}
+
+			for _, targetNs := range namespaces {
+				buf := new(bytes.Buffer)
+
+				cmd := exec.Command(kubectlBin, "exec", "-n"+originNs.name, originPods.Items[0].Name, "-csleep", "--",
+					"curl", fmt.Sprintf("http://httpbin.%s:8000/ip", targetNs.name), "-s", "-o", "/dev/null", "-w", "%{http_code}")
+				cmd.Stdout = buf
+				cmd.Stderr = GinkgoWriter
+				cmd.Run()
+
+				// if the origin pod has proxy, target pod has a proxy, we should expect 200
+				// if the origin pod has proxy, target does not, we should expect 200
+				// if the origin doesn't have proxy, target does, we should expect 000
+				// if the origin doesn't have proxy, target doesn't, we should expect 200
+
+				var badResult bool
+				if !originNs.inject && targetNs.inject {
+					badResult = buf.String() != "000"
+				} else {
+					badResult = buf.String() != "200"
+				}
+
+				if badResult {
+					Expect(fmt.Errorf("origin namespace %q has inject=%t, target namespace %q has inject=%t, but got curl response=%s",
+						originNs.name, originNs.inject, targetNs.name, targetNs.inject, buf.String(),
+					)).NotTo(HaveOccurred())
+				}
+			}
+		}
+	})
+})
