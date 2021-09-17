0337d66cee9bef9b0ebf8105e104d07fe678a199
joshvanl <vleeuwenjoshua@gmail.com>
2021-09-17 12:40:47 +0100

    Refactor controller to use metadata only cache, and be safer on namespace termination
    Signed-off-by: joshvanl <vleeuwenjoshua@gmail.com>

---
+++ pkg/controller/configmap.go
@@ -0,0 +1,209 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/cert-manager/istio-csr/pkg/tls"
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const (
+	configMapNameIstioRoot = "istio-ca-root-cert"
+	configMapLabelKey      = "istio.io/config"
+)
+
+type Options struct {
+	// LeaderElectionNamespace is the namespace that will be used to lease the
+	// leader election of each controller.
+	LeaderElectionNamespace string
+
+	// Log is the logger used by controllers.
+	Log logr.Logger
+
+	// TLS is the tls provider that is used for updating the config map.
+	TLS tls.Interface
+}
+
+// configmap is the controller that is responsible for ensuring that all
+// namespaces have the correct ConfigMap with the istio root CA.
+type configmap struct {
+	// client is a Kubernetes client that makes calls to the API for every
+	// request.
+	// Should be used for creating and updating resources.
+	client client.Client
+
+	// lister makes requests to the informer cache. Beware that resources who's
+	// informer only caches metadata, will not return underlying data of that
+	// resource. Use client instead.
+	lister client.Reader
+
+	// log is the logger used by configmap.
+	log logr.Logger
+
+	// tls provides the RootCA data that is propagated.
+	tls tls.Interface
+}
+
+func AddConfigMapController(ctx context.Context, mgr manager.Manager, opts Options) error {
+	c := &configmap{
+		client: mgr.GetClient(),
+		lister: mgr.GetCache(),
+		log:    opts.Log.WithName("controller").WithName("configmap"),
+		tls:    opts.TLS,
+	}
+
+	// Only reconcile config maps that match the well known name
+	if err := ctrl.NewControllerManagedBy(mgr).
+		// Reconcile ConfigMaps but only cache metadata
+		For(new(corev1.ConfigMap), builder.OnlyMetadata, builder.WithPredicates(predicate.NewPredicateFuncs(func(obj client.Object) bool {
+			// Only process ConfigMaps with the istio configmap name
+			return obj.GetName() == configMapNameIstioRoot
+		}))).
+
+		// Watch all Namespaces. Cache whole Namespace to include Phase Status.
+		Watches(&source.Kind{Type: new(corev1.Namespace)}, handler.EnqueueRequestsFromMapFunc(
+			func(obj client.Object) []reconcile.Request {
+				return []reconcile.Request{reconcile.Request{types.NamespacedName{Namespace: obj.GetName(), Name: configMapNameIstioRoot}}}
+			},
+		)).
+
+		// If the CA roots change then reconcile all ConfigMaps
+		Watches(&source.Channel{Source: c.tls.SubscribeRootCAsEvent()}, handler.EnqueueRequestsFromMapFunc(
+			func(obj client.Object) []reconcile.Request {
+				var namespaceList corev1.NamespaceList
+				if err := c.lister.List(ctx, &namespaceList); err != nil {
+					c.log.Error(err, "failed to list namespaces, exiting...")
+					os.Exit(0)
+				}
+				var requests []reconcile.Request
+				for _, namespace := range namespaceList.Items {
+					requests = append(requests, ctrl.Request{NamespacedName: types.NamespacedName{Namespace: namespace.Name, Name: configMapNameIstioRoot}})
+				}
+				return requests
+			},
+		)).
+
+		// Complete controller.
+		Complete(c); err != nil {
+
+		return fmt.Errorf("failed to create ConfigMap controller: %s", err)
+	}
+
+	return nil
+}
+
+// Reconcile is the main ConfigMap Reconcile loop. It will ensure that the
+// istio ConfigMap exists, and has the correct CA entry.
+func (c *configmap) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := c.log.WithValues("namespace", req.Namespace, "configmap", configMapNameIstioRoot)
+	log.V(3).Info("syncing configmap")
+
+	// Check Namespace is not deleted and is not in a terminating state.
+	var namespace corev1.Namespace
+	err := c.lister.Get(ctx, client.ObjectKey{Name: req.Namespace}, &namespace)
+	if apierrors.IsNotFound(err) {
+		// No need to reconcile the configmap if the namespace is deleted.
+		log.V(3).Info("namespace does not exist")
+		return ctrl.Result{}, nil
+	}
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if namespace.Status.Phase == corev1.NamespaceTerminating {
+		log.V(2).WithValues("phase", corev1.NamespaceTerminating).Info("skipping sync for namespace as it is terminating")
+		return ctrl.Result{}, nil
+	}
+
+	// Check ConfigMap exists, and has the correct data.
+	var configMap corev1.ConfigMap
+	err = c.client.Get(ctx, client.ObjectKey{Namespace: req.Namespace, Name: configMapNameIstioRoot}, &configMap)
+
+	// If the ConfigMap doesn't exist, create it with the correct data
+	if apierrors.IsNotFound(err) {
+		log.Info("creating configmap with root CA data")
+		return ctrl.Result{}, c.client.Create(ctx, &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      configMapNameIstioRoot,
+				Namespace: req.Namespace,
+				Labels:    map[string]string{configMapLabelKey: "true"},
+			},
+			Data: map[string]string{
+				"root-cert.pem": string(c.tls.RootCA()),
+			},
+		})
+	}
+
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if configMap.Data == nil {
+		configMap.Data = make(map[string]string)
+	}
+	if configMap.Labels == nil {
+		configMap.Labels = make(map[string]string)
+	}
+
+	var updateNeeded bool
+
+	// If the ConfigMap has unexpected keys, delete them
+	for k := range configMap.Data {
+		if k != "root-cert.pem" {
+			updateNeeded = true
+			configMap.Data = make(map[string]string)
+			break
+		}
+	}
+
+	// If the ConfigMap doesn't have the expected key value, update.
+	if data, ok := configMap.Data["root-cert.pem"]; !ok || data != string(c.tls.RootCA()) {
+		configMap.Data["root-cert.pem"] = string(c.tls.RootCA())
+		updateNeeded = true
+	}
+
+	// If the ConfigMap doesn't have the expected label, update.
+	if v, ok := configMap.Labels[configMapLabelKey]; !ok || v != "true" {
+		configMap.Labels[configMapLabelKey] = "true"
+		updateNeeded = true
+	}
+
+	if updateNeeded {
+		log.V(3).Info("updating ConfigMap")
+		return ctrl.Result{}, c.client.Update(ctx, &configMap)
+	}
+
+	log.V(3).Info("no update needed")
+
+	return ctrl.Result{}, nil
+}
---
+++ pkg/controller/configmap_test.go
@@ -0,0 +1,247 @@
+/*
+Copyright 2021 The cert-manager Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	tlsfake "github.com/cert-manager/istio-csr/pkg/tls/fake"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Reconcile(t *testing.T) {
+	const rootCAData = "root-ca"
+
+	tests := map[string]struct {
+		existingObjects []runtime.Object
+		expResult       ctrl.Result
+		expError        bool
+		expObjects      []runtime.Object
+	}{
+		"if namespace doesn't exist, ignore": {
+			existingObjects: nil,
+			expResult:       ctrl.Result{},
+			expError:        false,
+			expObjects:      nil,
+		},
+		"if namespace is in a terminating state, ignore": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{
+					TypeMeta:   metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"},
+					Status:     corev1.NamespaceStatus{Phase: corev1.NamespaceTerminating},
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{
+					TypeMeta:   metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"},
+					Status:     corev1.NamespaceStatus{Phase: corev1.NamespaceTerminating},
+				},
+			},
+		},
+		"if namespace exists, but configmap doesn't, create config map": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "1", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+		"if namespace and configmap exists, but doesn't have any data, update with data": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       nil,
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "11", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+		"if namespace and configmap exists, but doesn't have the right data, update with data": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": "not-root-ca"},
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "11", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+		"if namespace and configmap exists with correct data but with extra keys, remove extra keys": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData, "foo": "bar"},
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "11", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+		"if namespace and configmap exists with correct data but wrong label, update with correct label": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "false"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData, "foo": "bar"},
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "11", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+		"if namespace and configmap exists with correct data, do nothing": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+		"if namespace and configmap exists with correct data and extra labels, do nothing": {
+			existingObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true", "foo": "bar"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+			expResult: ctrl.Result{},
+			expError:  false,
+			expObjects: []runtime.Object{
+				&corev1.Namespace{TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"}, ObjectMeta: metav1.ObjectMeta{Name: "test-ns", ResourceVersion: "10"}},
+				&corev1.ConfigMap{
+					TypeMeta:   metav1.TypeMeta{Kind: "ConfigMap", APIVersion: "v1"},
+					ObjectMeta: metav1.ObjectMeta{Name: "istio-ca-root-cert", Namespace: "test-ns", ResourceVersion: "10", Labels: map[string]string{"istio.io/config": "true", "foo": "bar"}},
+					Data:       map[string]string{"root-cert.pem": rootCAData},
+				},
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			fakeclient := fakeclient.NewClientBuilder().
+				WithScheme(scheme.Scheme).
+				WithRuntimeObjects(test.existingObjects...).
+				Build()
+
+			c := &configmap{
+				client: fakeclient,
+				lister: fakeclient,
+				log:    klogr.New(),
+				tls:    tlsfake.New().WithRootCA([]byte(rootCAData)),
+			}
+
+			result, err := c.Reconcile(context.TODO(), ctrl.Request{NamespacedName: types.NamespacedName{Namespace: "test-ns", Name: "istio-ca-root-cert"}})
+			assert.Equalf(t, test.expError, err != nil, "%v", err)
+			assert.Equal(t, test.expResult, result)
+
+			for _, expectedObject := range test.expObjects {
+				expObj := expectedObject.(client.Object)
+				var actual client.Object
+				switch expObj.(type) {
+				case *corev1.ConfigMap:
+					actual = &corev1.ConfigMap{}
+				case *corev1.Namespace:
+					actual = &corev1.Namespace{}
+				default:
+					t.Errorf("unexpected object kind in expected: %#+v", expObj)
+				}
+
+				err := fakeclient.Get(context.TODO(), client.ObjectKeyFromObject(expObj), actual)
+				if err != nil {
+					t.Errorf("unexpected error getting expected object: %s", err)
+				} else if !apiequality.Semantic.DeepEqual(expObj, actual) {
+					t.Errorf("unexpected expected object, exp=%#+v got=%#+v", expObj, actual)
+				}
+			}
+		})
+	}
+}
