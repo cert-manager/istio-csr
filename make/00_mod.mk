076aa38cb0f660a59cc89d020fae7ec63a6847b8
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2024-01-09 18:58:22 +0100

    preload images used in e2e tests
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- make/00_mod.mk
+++ make/00_mod.mk
@@ -39,3 +39,12 @@ $(YQ) \
 	( .image.tag = "$(oci_manager_image_tag)" )' \
 	$1 --inplace
 endef
+
+mages_amd64 ?=
+images_arm64 ?=
+
+images_amd64 += docker.io/kong/httpbin:0.1.0@sha256:9d65a5b1955d2466762f53ea50eebae76be9dc7e277217cd8fb9a24b004154f4
+images_arm64 += docker.io/kong/httpbin:0.1.0@sha256:c546c8b06c542b615f053b577707cb72ddc875a0731d56d0ffaf840f767322ad
+
+images_amd64 += quay.io/curl/curl:8.5.0@sha256:f60b4d978aad8920d603df74bdd430b3ebe4895d9e06bc16125f897b168a699b
+images_arm64 += quay.io/curl/curl:8.5.0@sha256:a96e5f0e17b6e9699a76ce9f67a1412aec37fde5d881ed382f0295b97395e2ee
